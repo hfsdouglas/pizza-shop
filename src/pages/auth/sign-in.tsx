7fb924c82468690a5b25e35968adbfb60dce4f9b
Douglas Faria <hfsdouglas@gmail.com>
2024-11-21 21:28:41 -0300

    feat: page relative, new store button, and toast

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,14 +1,44 @@
+import { z } from "zod";
+import { toast } from "sonner";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { Helmet } from "react-helmet-async";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Helmet } from "react-helmet-async";
+
+const SignInFormSchema = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof SignInFormSchema>;
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      console.log(data);
+      toast.success("Enviamos um link de autenticação para seu e-mail.");
+    } catch (error) {
+      toast.error("Ocorreu um erro ao tentar realizar o login.");
+    }
+  }
+
   return (
     <>
       <Helmet title="Login" />
 
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -19,13 +49,13 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register("email")} />
             </div>
 
-            <Button type="submit" className="w-full">
+            <Button type="submit" className="w-full" disabled={isSubmitting}>
               Acessar painel
             </Button>
           </form>
