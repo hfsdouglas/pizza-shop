333764c588913ffd6eb04dbb2235c4217f14b949
Douglas Faria <hfsdouglas@gmail.com>
2024-11-21 22:39:26 -0300

    feat: add theme

--- src/app.tsx
+++ src/app.tsx
@@ -3,13 +3,16 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
 
 import { router } from "./routes";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
-      <Toaster richColors />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
