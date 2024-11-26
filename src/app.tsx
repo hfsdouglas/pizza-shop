5b4aad372b4e4927b41c84611ddce3e5cab71cb4
Douglas Faria <hfsdouglas@gmail.com>
2024-11-26 20:27:42 -0300

    feat: add react query provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,20 @@
 import { RouterProvider } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
 
 import { router } from "./routes";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
     <HelmetProvider>
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <Helmet titleTemplate="%s | pizza.shop" />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
         <Toaster richColors />
       </ThemeProvider>
     </HelmetProvider>
