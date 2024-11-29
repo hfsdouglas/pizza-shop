65c88354fb4ea22b98de7912fda69972d66aea47
Douglas Faria <hfsdouglas@gmail.com>
2024-11-28 21:40:06 -0300

    feat: updating restaurant info and setting stale time of queries

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -19,12 +19,14 @@ export function AccountMenu() {
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ["profile"],
     queryFn: getProfile,
+    staleTime: Infinity,
   });
 
   const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
     useQuery({
       queryKey: ["menaged-restaurant"],
       queryFn: getManagedRestaurant,
+      staleTime: Infinity,
     });
 
   return (
--- src/components/restaurant-profile-dialog.tsx
+++ src/components/restaurant-profile-dialog.tsx
@@ -1,6 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
+import { z } from "zod";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
 import { Button } from "./ui/button";
 import {
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -10,22 +16,52 @@ import {
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
+
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { updateProfile } from "@/api/update-profile";
 
 const RestaurantProfileSchema = z.object({
   name: z.string().min(1),
   description: z.string(),
 });
 
+type RestaurantProfile = z.infer<typeof RestaurantProfileSchema>;
+
 export function RestaurantProfileDialog() {
   const { data: managedRestaurant } = useQuery({
     queryKey: ["menaged-restaurant"],
     queryFn: getManagedRestaurant,
+    staleTime: Infinity,
   });
 
-  const {} = useForm({});
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm({
+    resolver: zodResolver(RestaurantProfileSchema),
+    values: {
+      name: managedRestaurant?.name ?? "",
+      description: managedRestaurant?.description ?? "",
+    },
+  });
+
+  const { mutateAsync: updateProfileFn } = useMutation({
+    mutationFn: updateProfile,
+  });
+
+  async function handleUpdateProfile(data: RestaurantProfile) {
+    try {
+      await updateProfileFn({
+        name: data.name,
+        description: data.description,
+      });
+
+      toast.success("Perfil atualizado com sucesso!");
+    } catch (error) {
+      toast.error("Falha ao atualizar o perfil. Tente novamente!");
+    }
+  }
 
   return (
     <DialogContent>
@@ -37,28 +73,35 @@ export function RestaurantProfileDialog() {
         </DialogDescription>
       </DialogHeader>
 
-      <form>
+      <form onSubmit={handleSubmit(handleUpdateProfile)}>
         <div className="space-y-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-right" htmlFor="name">
               Nome
             </Label>
-            <Input className="col-span-3" id="name" />
+            <Input className="col-span-3" id="name" {...register("name")} />
           </div>
 
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-right" htmlFor="description">
               Descrição
             </Label>
-            <Textarea className="col-span-3" id="description" />
+            <Textarea
+              className="col-span-3"
+              id="description"
+              {...register("description")}
+            />
           </div>
         </div>
 
         <DialogFooter>
-          <Button type="button" variant="ghost">
-            Cancelar
-          </Button>
-          <Button type="submit" variant="success">
+          <DialogClose asChild>
+            <Button type="button" variant="ghost">
+              Cancelar
+            </Button>
+          </DialogClose>
+
+          <Button type="submit" variant="success" disabled={isSubmitting}>
             Salvar
           </Button>
         </DialogFooter>
