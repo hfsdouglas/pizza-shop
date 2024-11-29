94efdbed50182b996a8f57a485b87f44ed6d7f56
Douglas Faria <hfsdouglas@gmail.com>
2024-11-28 21:58:50 -0300

    fix: corrects query key

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -24,7 +24,7 @@ export function AccountMenu() {
 
   const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
     useQuery({
-      queryKey: ["menaged-restaurant"],
+      queryKey: ["managed-restaurant"],
       queryFn: getManagedRestaurant,
       staleTime: Infinity,
     });
