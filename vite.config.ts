297b4e7c3d6dc8c9e2acf5b2ace8534a86bc97fe
Douglas Faria <hfsdouglas@gmail.com>
2024-12-05 20:27:31 -0300

    feat: vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+import type { UserConfig } from 'vite'
+import type { InlineConfig } from 'vitest/node'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +14,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+  test: {
+    globals: true,
+  },
+} as UserConfig & { test: InlineConfig })
