c752bb0aec8b7b854b12f372a57a7a2a1ece4aa8
Douglas Faria <hfsdouglas@gmail.com>
2025-01-02 20:25:38 -0300

    feat: add mocks imports

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -13,6 +13,10 @@ import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
 import { updateProfileMock } from './update-profile-mock'
 import { getOrdersMock } from './get-orders-mock'
 import { getOrderDetailsMock } from './get-orders-details-mock'
+import { approveOrderMock } from './approve-order-mock'
+import { cancelOrderMock } from './cancel-order-mock'
+import { deliverOrderMock } from './deliver-order-mock'
+import { dispatchOrderMock } from './dispatch-order-mock'
 
 export const worker = setupWorker(
   signInMock,
@@ -27,7 +31,11 @@ export const worker = setupWorker(
   getManagedRestaurantMock,
   updateProfileMock,
   getOrdersMock,
-  getOrderDetailsMock
+  getOrderDetailsMock,
+  approveOrderMock,
+  cancelOrderMock,
+  deliverOrderMock,
+  dispatchOrderMock
 )
 
 export async function enableMSW() {
