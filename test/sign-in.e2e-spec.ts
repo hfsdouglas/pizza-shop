3ddfd6c460d81ae39c6e89a44cb191e1111cbc95
Douglas Faria <hfsdouglas@gmail.com>
2025-01-02 21:01:52 -0300

    feat: add sign-in with wrong credentials test and navigate to new restaurant page

--- test/sign-in.e2e-spec.ts
+++ test/sign-in.e2e-spec.ts
@@ -12,3 +12,22 @@ test('sign-in successfully', async ({ page }) => {
 
   await expect(toast).toBeVisible()
 })
+
+test('sign-in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('e-mail').fill('wrong@example.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Ocorreu um erro ao tentar realizar o login.')
+
+  await expect(toast).toBeVisible()
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
+
+  await expect(page.url()).toContain('/sign-up')
+})
